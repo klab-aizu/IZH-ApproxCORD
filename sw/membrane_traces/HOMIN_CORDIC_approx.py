91e71ab8c3007262d55b2ab25497f5e81c8652e6
luyenvanvu <vudatai511@gmail.com>
2026-01-01 12:37:49 +0700

    Update HOMIN_CORDIC_approx.py

--- sw/membrane_traces/HOMIN_CORDIC_approx.py
+++ sw/membrane_traces/HOMIN_CORDIC_approx.py
@@ -108,7 +108,7 @@ def simulate_homin_neuron(d_float, iterations=12, t_max=1000, dt=0.03125):
 def plot_4_homin_behaviors():
     configs = [
         (8.0, "Regular Spiking"),
-        (5.0, "Intrinsically Bursting"),
+        (5.0, "Initial Bursting"),
         (1.125, "Chattering"),
         (0.375, "Low-Threshold Spiking")
     ]
